200b9987ad81d2011d5a8025f904cb8cee413ab1
Kento Sato <kento.s.account@gmail.com>
2016-09-06 13:31:03 -0700

    update TODO

--- autoclean.sh
+++ autoclean.sh
@@ -1,12 +1,12 @@
-make -k maintainer-clean
-rm ./config.h.in
-rm ./aclocal.m4
-rm ./config.status
-rm ./config.log
-rm -r autom4te.cache
-rm -r build-aux
-cd m4
-rm libtool.m4  ltoptions.m4  ltsugar.m4  ltversion.m4  lt~obsolete.m4
-cd -
+make -k maintainer-clean 2> /dev/null
+rm ./config.h.in 2> /dev/null
+rm ./aclocal.m4 2> /dev/null
+rm ./config.status 2> /dev/null
+rm ./config.log 2> /dev/null
+rm -r autom4te.cache 2> /dev/null
+rm -r build-aux 2> /dev/null
+cd m4 2> /dev/null
+rm libtool.m4  ltoptions.m4  ltsugar.m4  ltversion.m4  lt~obsolete.m4 2> /dev/null
+cd - 2> /dev/null
 
 
