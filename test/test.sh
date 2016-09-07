ce60e69373be487c2aad4126d537125d2b279b12
Kento Sato <kento.s.account@gmail.com>
2016-09-07 11:21:24 -0700

    update makefile.am

--- test/test.sh
+++ test/test.sh
@@ -10,6 +10,11 @@ mkdir ${dir}
 #io_watchdog="--io-watchdog"
 librempi=/g/g90/sato5/repo/rempi/install/lib/librempi.so
 
+bin="./rempi_test_master_worker"
+librempi=/g/g90/sato5/repo/rempi/install/lib/librempix.so
+REMPI_MODE=${mode} REMPI_DIR=${dir} REMPI_ENCODE=4 REMPI_GZIP=1 REMPI_TEST_ID=1 LD_PRELOAD=${librempi} srun -n ${num_procs} ${bin}
+exit
+
 
 bin="./rempi_test_msg_race 0 1 10000 2 0"
 REMPI_MODE=${mode} REMPI_DIR=${dir} REMPI_ENCODE=0 REMPI_GZIP=1 REMPI_TEST_ID=0 LD_PRELOAD=${librempi} srun --io-watchdog=conf=.io-watchdogrc -n ${num_procs} ${bin}
