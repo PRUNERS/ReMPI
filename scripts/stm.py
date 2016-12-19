6938a27151de998ac1095ea25d768f0371447eaf
Kento Sato <sato5@llnl.gov>
2016-12-18 18:21:49 -0800

    added stm.py

--- scripts/stm.py
+++ scripts/stm.py
@@ -1,58 +1,149 @@
-#!/bin/env python
+#!/usr/bin/env python
 import sys
 
-def extract_event(line):
+try_count = 0
+broken_count = 0
+matched_count = 0
+def extract_event(rank, line):
+    global try_count
+    global broken_line
+    try_count += 1
     vals = line.split(":")
-    if vals[4].strip() == "STM":
-        dic = {}
-        dic["id"] = vals[3]
-        dic["type"] = vals[5]
-        dic["rank"] = vals[6]
-        dic["tag"] = vals[7]
-        dic["st"] = vals[8]
-        return dic
-    else:
+    try:
+        if vals[0].strip() == "STM":
+            dic = {}
+            dic["id"] = rank
+            if not (0 <= dic["id"] and dic["id"] <= 63):
+                print "Format error", vals
+                exit(1)
+            dic["type"] = vals[1]
+            if not dic["type"] == "Send" and not dic["type"] == "MF":
+                print "Format error", vals
+                exit(1)
+            dic["rank"] = int(vals[2])
+            if not (0 <= dic["rank"] and dic["rank"] <= 63):
+                print "Format error", vals
+                exit(1)
+            dic["tag"] = int(vals[3])
+            dic["st"] = vals[4]
+            return dic
+        else:
+            return None
+    except:
+        print vals
+        broken_count += 1
         return None
-
-event_cache = []
+    
+matched_event = {}
+def add_matched_event(send_event, recv_event):
+    global matched_count
+    matched_count += 1
+    tuple  = (send_event["st"], recv_event["st"])
+    if matched_event.has_key(tuple) == False:
+        matched_event[tuple] = 1
+    else:
+        matched_event[tuple] = matched_event[tuple] + 1
+    
 
 def message_matching(e1, e2):
-    if e1["type"] == "Send" && e2["type"] == "MF":
+    if e1["type"] == "Send" and e2["type"] == "MF":
         send_event = e1
         recv_event = e2
-    elif e1["type"] == "MF" && e2["type"] == "Send":
+    elif e1["type"] == "MF" and e2["type"] == "Send":
         send_event = e2
         recv_event = e1
     else:
         return False
 
-def match_message_in_cache(event):
-    for e in event_cache:
-        if event["type"] == e["type"]:
-            continue
-        message_matching(event, e)
+    if send_event["rank"] == recv_event["id"] and send_event["id"] == recv_event["rank"] and send_event["tag"] == recv_event["tag"]:
+        add_matched_event(send_event, recv_event)
+        return True
+    else:
+        return False
+ 
+read_size = 0
+def read_event(rank):
+    global read_size
+    line = fds[rank].readline()
+    read_size += len(line)
+    if line == "":
+        return None
+    else:
+        return extract_event(rank, line)
+
+fds = []
+def open_files(num):
+    global fds
+    for i in range(num):
+        fd = open("./rank_" + str(i) + ".stm")
+        fds.append(fd)
 
 
-def match_message(fd, event):
-    match_message_in_cache(event)
+def pop_next_event(rank):
+    next_event = None
+    event_cache = event_caches[rank]
+    if len(event_cache) > 0:
+        next_event = event_cache.pop(0)
+    else:
+        next_event = read_event(rank)
+    return next_event
+
+def event_matching(event, target_rank):
+    target_event_cache = event_caches[target_rank]
+    for target_event in target_event_cache:
+        is_matched = message_matching(event, target_event)
+        if is_matched: 
+            target_event_cache.remove(target_event)
+            return True
+    target_event = read_event(target_rank)
+    while not target_event == None:
+        is_matched = message_matching(event, target_event)
+
+        if is_matched == True: return True
+        event_caches[target_rank].append(target_event)
+        target_event = read_event(target_rank)
+    return False
+
+orphans=[]
+
+event_caches = None
+def match_message_of(rank):
+    event = pop_next_event(rank)
+
+    if event == None:
+        return True
+    is_matched = event_matching(event, event["rank"])
+    if is_matched == False:
+        orphans.append(event)
     
+def sum_cache():
+    sum = 0
+    for event_cache in event_caches:
+        sum += len(event_cache)
+    return sum
 
 def main():
-    # my code here
-    if len(sys.argv) == 3:
-        input_path = sys.argv[1]
-        output_path = sys.argv[2]
-    elif len(sys.argv) == 2:
-        input_path = sys.argv[1]
-        output_path = None
+    global event_caches
+    global read_size
+    global matched_count
+    num_rank = 64
+    event_caches = [[] for j in range(num_rank)]
 
-    fd = open(input_path, "r")
-    for line in fd:
-        event = extract_event(line);
-        if event == None: 
-            continue
-        match_message(fd, event)
+    open_files(num_rank)
 
+
+    while True:
+        done_count = 0
+        for rank in range(num_rank):
+            done = match_message_of(rank)
+            if done == True: done_count += 1
+        if done_count == num_rank:
+            break
+        print float(read_size) / 1000000000, "GB", matched_count, done_count, broken_count, sum_cache()
+
+    print "result"            
+    print matched_event
+    print len(orphans)
 
 if __name__ == "__main__":
     main()
