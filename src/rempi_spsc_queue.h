2e4c9d65b4854b6869c299f238078e5cd79198bd
Kento Sato <kento.s.account@gmail.com>
2016-12-28 15:50:41 -0800

    Added LICENSE.TXT and copyright description

--- src/rempi_spsc_queue.h
+++ src/rempi_spsc_queue.h
@@ -1,10 +1,26 @@
-/*
- * rempi_event_list2.h
- *
- *  Created on: Nov 8, 2014
- *      Author: sato5
- */
-
+/* ==========================ReMPI:LICENSE==========================================   
+   Copyright (c) 2016, Lawrence Livermore National Security, LLC.                     
+   Produced at the Lawrence Livermore National Laboratory.                            
+                                                                                       
+   Written by Kento Sato, kento@llnl.gov. LLNL-CODE-711357.                           
+   All rights reserved.                                                               
+                                                                                       
+   This file is part of ReMPI. For details, see https://github.com/PRUNER/ReMPI       
+   Please also see the LICENSE file for our notice and the LGPL.                      
+                                                                                       
+   This program is free software; you can redistribute it and/or modify it under the   
+   terms of the GNU General Public License (as published by the Free Software         
+   Foundation) version 2.1 dated February 1999.                                       
+                                                                                       
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY    
+   WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or                  
+   FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU          
+   General Public License for more details.                                           
+                                                                                       
+   You should have received a copy of the GNU Lesser General Public License along     
+   with this program; if not, write to the Free Software Foundation, Inc., 59 Temple   
+   Place, Suite 330, Boston, MA 02111-1307 USA
+   ============================ReMPI:LICENSE========================================= */
 #ifndef REMPI_SPSC_QUEUE_H_
 #define REMPI_SPSC_QUEUE_H_
 
--- test/msub.sh
+++
@@ -1,9 +0,0 @@
-nodes=4
-min="15:00"
-
-for i in `seq 30`
-do
-command="msub -l nodes=${nodes} -l walltime=${min} -N hypre${i} ./run.sh"
-echo $command
-$command
-done
